899f930f21bd972b706816ae6e97a53f1939c16d
github username <github email address>
2018-10-23 17:11:14 -0400

    trying to fix ActionCable url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,7 +83,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.web_socket_server_url = "wss://hatch-backend.herokuapp.com/cable"
+  # config.web_socket_server_url = "wss://hatch-backend.herokuapp.com/cable"
 
   config.action_cable.url = "wss://hatch-backend.herokuapp.com/cable"
 
