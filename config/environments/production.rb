c967d6608ccf8b0edbfc89e88d9405eec88c14f0
github username <github email address>
2018-10-23 16:07:50 -0400

    configured REDISTOGOURL, WebSocket server url, and ActionCable allowed request origins for Heroku deployment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,4 +82,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.web_socket_server_url = "wss://hatch-backend.herokuapp.com/cable"
+
+  config.action_cable.allowed_request_origins = ['https://hatch-backend.herokuapp.com', 'http://hatch-backend.herokuapp.com']
 end
